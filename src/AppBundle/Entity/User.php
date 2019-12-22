9953d6f5766ae854c9059031dc4021c0e6756429
zitou54 <46718890+zitou54@users.noreply.github.com>
2019-12-22 19:23:46 +0100

    fos UserBundel nom de la table => bd_zacc

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zitouni
+ * Date: 22/12/2019
+ * Time: 19:00
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
