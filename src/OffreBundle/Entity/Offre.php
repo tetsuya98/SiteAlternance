65d49629c377bb0623e442a2087a7dac7c73a6e9
Valentin Cocherel <valentin.cocherel@gmail.com>
2020-02-02 15:29:59 +0100

    Mise en place des pièces jointes

--- src/OffreBundle/Entity/Offre.php
+++ src/OffreBundle/Entity/Offre.php
@@ -360,4 +360,67 @@ class Offre
     {
         return $this->imageSize;
     }
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="offre_file", fileNameProperty="fileName", size="fileSize")
+     *
+     * @var File
+     */
+    protected $file;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     *
+     * @var string
+     */
+    protected  $fileName;
+
+    /**
+     * @ORM\Column(type="integer",nullable=true)
+     *
+     * @var integer
+     */
+    protected  $fileSize;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile $file
+     * @throws Exception
+     */
+    public function setFile(?File $file = null): void
+    {
+        $this->file = $file;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFileName(?string $imageName): void
+    {
+        $this->fileName = $imageName;
+    }
+
+    public function getFileName(): ?string
+    {
+        return $this->fileName;
+    }
+
+    public function setFileSize(?int $fileSize): void
+    {
+        $this->fileSize = $fileSize;
+    }
+
+    public function getFileSize(): ?int
+    {
+        return $this->fileSize;
+    }
 }
