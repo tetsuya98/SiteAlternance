9cb369fea7bbb1cb552ee29437057b6c5d529822
Valentin Cocherel <valentin.cocherel@gmail.com>
2020-02-02 13:14:50 +0100

    Merge branch 'master' of C:\UwAmp\www\Conduite_Projet with conflicts.

--- src/OffreBundle/Entity/Offre.php
+++ src/OffreBundle/Entity/Offre.php
@@ -2,13 +2,19 @@
 
 namespace OffreBundle\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Offre
  *
  * @ORM\Table(name="offre")
  * @ORM\Entity(repositoryClass="OffreBundle\Repository\OffreRepository")
+ * @Vich\Uploadable()
  */
 class Offre
 {
@@ -44,6 +50,13 @@ class Offre
     private $titre;
 
     /**
+     * @var TypeContrat
+     *
+     * @ORM\ManyToOne(targetEntity="OffreBundle\Entity\TypeContrat")
+     */
+    private $typeContrat;
+
+    /**
      * @var string
      *
      * @ORM\Column(name="description", type="text")
@@ -51,7 +64,7 @@ class Offre
     private $description;
 
     /**
-     * @var \DateTime
+     * @var DateTime
      *
      * @ORM\Column(name="crdate", type="datetime")
      */
@@ -133,7 +146,7 @@ class Offre
     /**
      * Set crdate
      *
-     * @param \DateTime $crdate
+     * @param DateTime $crdate
      *
      * @return Offre
      */
@@ -147,7 +160,7 @@ class Offre
     /**
      * Get crdate
      *
-     * @return \DateTime
+     * @return DateTime
      */
     public function getCrdate()
     {
@@ -260,5 +273,122 @@ class Offre
             return;
         }
         $this->competences->removeElement($competence);
+    }
+
+    /**
+     * @return TypeContrat
+     */
+    public function getTypeContrat()
+    {
+        return $this->typeContrat;
+    }
+
+    /**
+     * @param TypeContrat $typeContrat
+     */
+    public function setTypeContrat(TypeContrat $typeContrat): void
+    {
+        $this->typeContrat = $typeContrat;
+    }
+
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="offre_image", fileNameProperty="imageName", size="imageSize")
+     *
+     * @var File
+     */
+    protected $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255,nullable=true)
+     *
+     * @var string
+     */
+    protected  $imageName;
+
+    /**
+     * @ORM\Column(type="integer",nullable=true)
+     *
+     * @var integer
+     */
+    protected  $imageSize;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile $imageFile
+     * @throws Exception
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
+    }
+
+    /**
+     * Set images
+     *
+     * @param string $images
+     *
+     * @return satelliteImage[]
+     */
+    public function setImages($images)
+    {
+        $this->files = $images;
+
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return string
+     */
+    public function getImages()
+    {
+        return $this->image;
+    }
+
+    public function addImage($image)
+    {
+        $this->images[] = $image;
+
+        return $this;
     }
 }
--- src/OffreBundle/Type/OffreType.php
+++ src/OffreBundle/Type/OffreType.php
@@ -3,24 +3,30 @@ namespace OffreBundle\Type;
 
 use AppBundle\Entity\Competence;
 use AppBundle\Repository\CompetenceRepository;
+use OffreBundle\Entity\TypeContrat;
+use OffreBundle\Repository\TypeContratRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
+//
 class OffreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre', TextType::class, ['label' => "Titre de l'offre"])
-            ->add('description', TextareaType::class, ['label' => "Description"])
+            ->add('imageFile', VichImageType::class, ['required' => false])
+            ->add('titre', TextType::class, ['label' => "Intitulé de l'offre"])
+            ->add('description', TextareaType::class, ['label' => "Description de l'offre"])
             ->add('nbSemaine', NumberType::class, ['label' => "Durée du stage (en semaine)"])
-            ->add('competences', EntityType::class, [
-                'label' => 'Compétences de l\'offre',
+            ->add('competences', EntityType::class,         [
+                'label' => 'Domaines et compétences',
                 'class' => Competence::class,
                 'choice_label' => 'competences',
                 'query_builder' => function (CompetenceRepository $rep) {
@@ -28,6 +34,22 @@ class OffreType extends AbstractType
                         ->orderBy('u.competences', 'ASC');
                 },
                 'multiple' => true,
+                'required' => false
+            ])
+            ->add('typeContrat', EntityType::class, [
+                'label' => 'Type de contrat',
+                'class' => TypeContrat::class,
+                'choice_label' => 'titre',
+                'query_builder' => function (TypeContratRepository $rep) {
+                    return $rep->createQueryBuilder('u')
+                        ->orderBy('u.titre', 'ASC');
+                }
+            ])
+            ->add('files', FileType::class, [
+                'multiple' => true,
+                'attr'     => [
+                    'multiple' => 'multiple'
+                ]
             ])
             ->add('submit', SubmitType::class, ['label' => "Publier l'offre"])
             ->getForm();
